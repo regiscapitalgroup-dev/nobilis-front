239fa3d49b8bb06ab8184334500227f2f908bca2
manuel.mendez <manuel.mendez@arcacontal.com>
2025-12-04 09:55:35 -0600

    Fix: handle undefined languages in MemberLanguages component
    - Added a conditional check to ensure the languages array is defined before mapping over it, preventing potential runtime errors.

--- src/app/pages/members/components/MemberLanguages.tsx
+++ src/app/pages/members/components/MemberLanguages.tsx
@@ -10,7 +10,7 @@ export const MemberLanguages: FC<MemberLanguagesProps> = ({languages}) => {
       <div className='member-detail__languages-inner'>
         <div className='member-detail__languages-title'>Language Spoken</div>
         <div className='member-detail__languages-tags'>
-          {languages.map((item, index) => (
+          {languages && languages.map((item, index) => (
             <div key={index} className='member-detail__language-tag'>
               <div className='member-detail__language-tag-text'>{item}</div>
             </div>
